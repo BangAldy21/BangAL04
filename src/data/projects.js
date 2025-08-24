e82c0478506daa069f9d518aaf74110217023222
BangAldy21 <anggachannel011@gmail.com>
2025-08-25 00:21:53 +0800

    Add files via upload

---
+++ src/data/projects.js
@@ -0,0 +1,8 @@
+export const projects = [
+  { title: 'Website Sekolah', img: '/src/assets/p1.jpg', stack: 'HTML CSS JavaScript AOS', link: '#' },
+  { title: 'Company Profile', img: '/src/assets/p2.jpg', stack: 'React + Swiper + Lightbox', link: '#' },
+  { title: 'Wedding Website 2.0', img: '/src/assets/p3.jpg', stack: 'Vite + Tailwind + AOS', link: '#' },
+  { title: 'Web Course', img: '/src/assets/p4.jpg', stack: 'Vite + React + Bootstrap', link: '#' },
+  { title: 'Web Portfolio', img: '/src/assets/p5.jpg', stack: 'HTML CSS Bootstrap', link: '#' },
+  { title: 'Company Profile 2.0', img: '/src/assets/p6.jpg', stack: 'NextJS + Tailwind + Framer Motion', link: '#' },
+]
---
+++ src/data/tools.js
@@ -0,0 +1,10 @@
+export const tools = [
+  { name: 'React', img: '/src/assets/react.svg' },
+  { name: 'Vite', img: '/src/assets/vite.svg' },
+  { name: 'Tailwind', img: '/src/assets/tailwind.svg' },
+  { name: 'Firebase', img: '/src/assets/firebase.svg' },
+  { name: 'GitHub', img: '/src/assets/github.svg' },
+  { name: 'Figma', img: '/src/assets/figma.svg' },
+  { name: 'Node.js', img: '/src/assets/node.svg' },
+  { name: 'TypeScript', img: '/src/assets/ts.svg' },
+]
